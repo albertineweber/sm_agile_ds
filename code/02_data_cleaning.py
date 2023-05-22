a90cd2bcae13b3ed8ed96b5f5beb0760fbd6c21d
albertineweber <albertine.wc@gmail.com>
2023-05-21 23:41:07 -0300

    adjust utf and duplicates reading

--- code/02_data_cleaning.py
+++ code/02_data_cleaning.py
@@ -12,24 +12,46 @@ latest_version = max(os.listdir(path_intermediate))
 execution_version = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 # read input_table
-consolidated_df = pd.read_csv("%s/search_table.csv" % (path_intermediate))
+consolidated_df = pd.read_csv(
+    "%s/search_table.csv" % (path_intermediate), encoding="UTF-16"
+)
 
-# sanitize title name
+# sanitize fields
 consolidated_df["sanitized_title"] = consolidated_df["title"].str.lower().str.split(" ")
+consolidated_df["sanitized_doi"] = consolidated_df["doi"].str.lower()
+consolidated_df["sanitized_abstract"] = (
+    consolidated_df["abstract"].str.lower().str.split(" ")
+)
 
 # build doi and title duplicated flags
 consolidated_df["duplicated_title"] = consolidated_df["sanitized_title"].duplicated()
-consolidated_df["duplicated_doi"] = (consolidated_df["doi"].duplicated()) & (
-    ~consolidated_df["doi"].isna()
+consolidated_df["duplicated_doi"] = (consolidated_df["sanitized_doi"].duplicated()) & (
+    ~consolidated_df["sanitized_doi"].isna()
 )
+consolidated_df["duplicated_abstract"] = (
+    consolidated_df["sanitized_abstract"].duplicated()
+) & (~consolidated_df["sanitized_abstract"].isna())
 
 # generate exclusion flag
 consolidated_df["duplicated_exclusion_flag"] = (
-    consolidated_df["duplicated_title"] | consolidated_df["duplicated_doi"]
+    consolidated_df["duplicated_title"]
+    | consolidated_df["duplicated_doi"]
+    | consolidated_df["duplicated_abstract"]
 )
+
 consolidated_df["duplicated_validation_date"] = execution_version
 
-consolidated_df = consolidated_df.drop(["duplicated_title", "duplicated_doi"], axis=1)
+consolidated_df = consolidated_df.drop(
+    [
+        "duplicated_title",
+        "duplicated_doi",
+        "duplicated_abstract",
+        "sanitized_title",
+        "sanitized_doi",
+        "sanitized_abstract",
+    ],
+    axis=1,
+)
 
 # create empty validation cols
 consolidated_df["metadata_exclusion_flag"] = np.nan
@@ -46,7 +68,10 @@ if not os.path.exists("%s/versioned/%s" % (path_primary, execution_version)):
 consolidated_df.to_csv(
     "%s/versioned/%s/cleaned_search_table.csv" % (path_primary, execution_version),
     index=False,
+    encoding="utf-16",
 )
 
 # save current version
-consolidated_df.to_csv("%s/cleaned_search_table.csv" % (path_primary), index=False)
+consolidated_df.to_csv(
+    "%s/cleaned_search_table.csv" % (path_primary), index=False, encoding="utf-16"
+)
