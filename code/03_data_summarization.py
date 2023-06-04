03b3dc7066b162ff5f61537db0f3c63cb726902a
albertineweber <albertine.wc@gmail.com>
2023-06-04 20:41:55 -0300

    add filled search table

--- code/03_data_summarization.py
+++ code/03_data_summarization.py
@@ -5,16 +5,29 @@ import numpy as np
 import pandas as pd
 
 # get most recent version
-path_primary = "data/03_primary"
 path_feature = "data/04_feature"
-latest_version = max(os.listdir(path_primary))
+path_mi = "data/05_model_input"
+latest_version = max(os.listdir(path_feature))
 # id of the current version: execution_version = ""
 execution_version = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 # read input_table
-consolidated_df = pd.read_csv(
-    "%s/cleaned_search_table.csv" % (path_primary), encoding="UTF-16"
+consolidated_df = pd.read_excel("%s/filled_search_table.xlsx" % (path_feature))
+
+# save copy of versioned dbs and current version
+if not os.path.exists("%s/versioned/%s" % (path_feature, execution_version)):
+    os.makedirs("%s/versioned/%s" % (path_feature, execution_version))
+
+consolidated_df.to_csv(
+    "%s/versioned/%s/filled_search_table.csv" % (path_feature, execution_version),
+    index=False,
+    encoding="utf-16",
 )
+
+consolidated_df.to_csv(
+    "%s/filled_search_table.csv" % (path_feature), index=False, encoding="utf-16"
+)
+
 
 # create dict of criterias
 criterias_dct = {
@@ -32,7 +45,6 @@ criterias_dct = {
     "NIC4": "Not IC4 - <2018",
 }
 
-
 # create exclusion criteria and date cols
 conditions = [
     consolidated_df["duplicated_exclusion_flag"] == 1,
@@ -40,7 +52,7 @@ conditions = [
     consolidated_df["fulltext_exclusion_flag"] == 1,
 ]
 criteria_choices = [
-    "EC0",
+    "EC0 - Duplicated entry",
     consolidated_df["metadata_exclusion_criteria"],
     consolidated_df["fulltext_exclusion_criteria"],
 ]
@@ -51,9 +63,53 @@ dates_choices = [
 ]
 
 consolidated_df["exclusion_criteria"] = np.select(
-    conditions, criteria_choices, default=np.nan
+    conditions, criteria_choices, default="Not excluded"
 )
-consolidated_df["exclusion_date"] = np.select(conditions, dates_choices, default=np.nan)
+consolidated_df["exclusion_date"] = np.select(conditions, dates_choices, default="N/A")
 
-# TODO: create logic for dummification of exclusion criterias used?
-# TODO: create cols for RQs
+# Save complete dataframe
+complete_df = consolidated_df.copy()
+
+# Create filtered copy with only selected studies
+selected_df = consolidated_df[consolidated_df["exclusion_criteria"] == "Not excluded"]
+
+cols_for_rqs = [
+    "RQ1_PubSourceType",
+    "RQ1_PubSourceName",
+    "RQ1_PubYear",
+    "RQ2_ResearchType",
+    "RQ3_ResearchMethod",
+    "RQ4_OrganizationType",
+    "RQ5_SubtopicsAM",
+    "RQ6_SubtopicsDS",
+    "RQ7_ProblemsReported",
+    "RQ8_ObjectiveAM",
+    "RQ9_ContributionsResults",
+]
+
+for col in cols_for_rqs:
+    selected_df[col] = np.nan
+
+# save copy of versioned dbs and current version
+if not os.path.exists("%s/versioned/%s" % (path_mi, execution_version)):
+    os.makedirs("%s/versioned/%s" % (path_mi, execution_version))
+
+complete_df.to_csv(
+    "%s/versioned/%s/complete_filled_search_table.csv" % (path_mi, execution_version),
+    index=False,
+    encoding="utf-16",
+)
+
+complete_df.to_csv(
+    "%s/complete_filled_search_table.csv" % (path_mi), index=False, encoding="utf-16"
+)
+
+selected_df.to_csv(
+    "%s/versioned/%s/selected_filled_search_table.csv" % (path_mi, execution_version),
+    index=False,
+    encoding="utf-16",
+)
+
+selected_df.to_csv(
+    "%s/selected_filled_search_table.csv" % (path_mi), index=False, encoding="utf-16"
+)
