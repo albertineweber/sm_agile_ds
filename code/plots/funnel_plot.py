2c8503a5eaf2bf5152a279230f3d6ba5c9747895
albertineweber <albertine.wc@gmail.com>
2023-06-04 22:44:23 -0300

    improve funnel plot

--- code/plots/funnel_plot.py
+++ code/plots/funnel_plot.py
@@ -49,11 +49,11 @@ for db in complete_df["database"].unique():
                 "Step 3 - Metadata screening",
                 "Step 4 - Full-text screening",
             ],
-            x=[step1, step2, step3, step4],
+            x=[step1, 0, 0, 0],
             textinfo="value",
             textfont=dict(
                 color="black",
-                size=20,  # can change the size of font here
+                size=22,  # can change the size of font here
                 family="Times New Roman",
             ),
             constraintext="outside",
@@ -69,58 +69,37 @@ step2 = step1 - len(filt_df[filt_df["exclusion_step"] == "Duplicates filter"])
 step3 = step2 - len(filt_df[filt_df["exclusion_step"] == "Metadata filter"])
 step4 = step3 - len(filt_df[filt_df["exclusion_step"] == "Full-text filter"])
 
+fig.add_trace(
+    go.Funnel(
+        y=[
+            "Step 1 - Before screening",
+            "Step 2 - Duplicates screening",
+            "Step 3 - Metadata screening",
+            "Step 4 - Full-text screening",
+        ],
+        x=[0, step2, step3, step4],
+        textinfo="value",
+        textfont=dict(
+            color="black",
+            size=22,  # can change the size of font here
+            family="Times New Roman",
+        ),
+        constraintext="outside",
+        marker=dict(line=dict(color="white")),
+        showlegend=False,
+    ),
+)
+
 fig.add_annotation(
     text=str(step1),
     showarrow=False,
     xref="paper",
     yref="paper",
-    x=0.98,
+    x=0.99,
     y=0.89,
     font=dict(
         color="black",
-        size=20,  # can change the size of font here
-        family="Times New Roman",
-    ),
-)
-
-fig.add_annotation(
-    text=str(step2),
-    showarrow=False,
-    xref="paper",
-    yref="paper",
-    x=0.82,
-    y=0.625,
-    font=dict(
-        color="black",
-        size=20,  # can change the size of font here
-        family="Times New Roman",
-    ),
-)
-
-fig.add_annotation(
-    text=str(step3),
-    showarrow=False,
-    xref="paper",
-    yref="paper",
-    x=0.61,
-    y=0.375,
-    font=dict(
-        color="black",
-        size=20,  # can change the size of font here
-        family="Times New Roman",
-    ),
-)
-
-fig.add_annotation(
-    text=str(step4),
-    showarrow=False,
-    xref="paper",
-    yref="paper",
-    x=0.54,
-    y=0.104,
-    font=dict(
-        color="black",
-        size=20,  # can change the size of font here
+        size=22,  # can change the size of font here
         family="Times New Roman",
     ),
 )
